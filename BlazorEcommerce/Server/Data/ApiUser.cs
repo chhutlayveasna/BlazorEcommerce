d609ae7ffe5c647670ff8a517bc3cb081e14688f
Chhut Lay Veasna <chhutlayveasna@gmail.com>
2022-05-14 12:58:07 +0700

    Add EntityFramework and Initial Model

---
+++ BlazorEcommerce/Server/Data/ApiUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerce.Server.Data
+{
+    public class ApiUser : IdentityUser
+    {
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [StringLength(255)]
+        public string LastName { get; set; }
+    }
+}
--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,14 +1,30 @@
 global using BlazorEcommerce.Shared;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add DbContext for Migrations
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+// Add Swagger Services (step 1)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Add Swagger UI (step 2)
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +38,8 @@ else
     app.UseHsts();
 }
 
+// use Swagger (step 3)
+app.UseSwagger();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
